a9d02697878b51a503c00b0927ef35334354059e
mynameisarando <tylercrowhurst123@gmail.com>
2019-06-16 07:45:36 +0100

    add hades

---
+++ other/hades/js/mobile.js
@@ -0,0 +1,49 @@
+// Start Animations
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".welcome").slideToggle("slow");
+		$(".container").slideToggle("slow");
+		$(".taskbarContainer").delay(600).slideToggle("slow");
+    }, 2000); //Default value = 2000
+});
+
+
+// Windows
+$(document).ready(function(){
+	$(".meButton").click(function(){
+		$(".meWindow").fadeToggle("fast");
+		$(".me1").toggleClass("active");
+	});
+});
+$(document).ready(function(){
+	$(".socialButton").click(function(){
+		$(".socialWindow").fadeToggle("slow");
+		$(".so1").toggleClass("active");
+	});
+});
+$(document).ready(function(){
+	$(".projectButton").click(function(){
+		$(".projectWindow").fadeToggle("slow");
+		$(".pro1").toggleClass("active");
+	});
+});
+
+// Apps
+$(document).ready(function(){
+	$(".meMax").click(function(){
+		$(".desktop").delay(350).fadeToggle("fast");
+		$(".meApp").delay(500).fadeToggle("fast");
+	});
+});
+$(document).ready(function(){
+	$(".proMax").click(function(){
+		$(".desktop").delay(350).fadeToggle("fast");
+		$(".projectApp").delay(500).fadeToggle("fast");
+	});
+});
+$(document).ready(function(){
+	$(".soMax").click(function(){
+		$(".desktop").delay(350).fadeToggle("fast");
+		$(".socialApp").delay(500).fadeToggle("fast");
+	});
+});
